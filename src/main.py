4725224e88c8aa1621486373f53068e07a39f913
Kacper Ochnik <kacperochnik01@gmail.com>
2024-01-10 22:39:34 +0100

    testing mnist

--- src/main.py
+++ src/main.py
@@ -1,61 +1,27 @@
-from data import DataPoint
+from data import DataPoint, MnistDataloader
 from neural_network import NeuralNetwork
 import matplotlib.pyplot as plt
 
 
 # Create a simple training dataset
-training_data = [
-    # AND function: y = x1 AND x2
-    [
-        DataPoint([0, 0], 0, 2),
-        DataPoint([0, 1], 0, 2),
-        DataPoint([1, 0], 0, 2),
-        DataPoint([1, 1], 1, 2),
-    ],
-    # OR function: y = x1 OR x2
-    [
-        DataPoint([0, 0], 0, 2),
-        DataPoint([0, 1], 1, 2),
-        DataPoint([1, 0], 1, 2),
-        DataPoint([1, 1], 1, 2),
-    ],
-    # XOR function: y = x1 XOR x2
-    [
-        DataPoint([0, 0], 0, 2),
-        DataPoint([0, 1], 1, 2),
-        DataPoint([1, 0], 1, 2),
-        DataPoint([1, 1], 0, 2),
-    ],
-]
+mnist = MnistDataloader(
+    "./input/train-images-idx3-ubyte/train-images-idx3-ubyte",
+    "./input/train-labels-idx1-ubyte/train-labels-idx1-ubyte",
+    "./input/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte",
+    "./input/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte",
+)
+mnist_data, mnist_test = mnist.load_data()
+images, labels = mnist_data
 
-# Test the neural network
-fig, axs = plt.subplots(len(training_data), 1, figsize=(5, 15))  # Create subplots for each test
+training_data = []
+for i in range(len(images)):
+    flattened_image = [pixel for sublist in images[i] for pixel in sublist]
+    training_data.append(DataPoint(flattened_image, labels[i], 10))
 
-for i, test in enumerate(training_data):
-    nn = NeuralNetwork(2, 10, 2)
-    learning_rate = 0.3
-    numberOfSteps = 1000
-    accuracies = []  # List to store accuracies
-
-    # Train the neural network
-    for _ in range(numberOfSteps):
-        nn.learn(test, learning_rate)
-        
-        # Calculate accuracy
-        correct_predictions = 0
-        for data_point in test:
-            inputs = data_point.inputs
-            expected_output = data_point.expected_outputs
-            actual_output = nn.calculate_outputs(inputs)
-            if (actual_output[0] > 0.5) == expected_output[0] and (actual_output[1] > 0.5) == expected_output[1]:
-                correct_predictions += 1
-        accuracy = correct_predictions / len(test)
-        accuracies.append(accuracy)
-
-    # Plot accuracy over iterations
-    axs[i].plot(accuracies)
-    axs[i].set_xlabel('Iteration')
-    axs[i].set_ylabel('Accuracy')
-
-plt.tight_layout()
-plt.show()
+nn = NeuralNetwork(784, 89, 10)
+learning_rate = 1
+numberOfSteps = 1000
+print("Start")
+for i in range(numberOfSteps):
+    nn.learn(training_data, learning_rate)
+    print("Step: ", i, " Cost: ", nn.total_cost(training_data))
