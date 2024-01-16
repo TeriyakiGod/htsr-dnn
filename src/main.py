293348856d1b96ded08cf7ce3e4e520fe41a5581
Kacper Ochnik <kacperochnik01@gmail.com>
2024-01-16 23:24:53 +0100

    merge fix

--- src/main.py
+++ src/main.py
@@ -16,16 +16,6 @@ mnist = MnistDataloader(
 mnist_data, mnist_test = mnist.load_data()
 images, labels = mnist_data
 
-training_data = []
-for i in range(1000):
-    flattened_image = [pixel for sublist in images[i] for pixel in sublist]
-    training_data.append(DataPoint(flattened_image, labels[i], 10))
-
-nn = NeuralNetwork(784, 2, 10)
-learning_rate = 0.9
-batch_size = 128
-numberOfSteps = 1
-batches = []
 nn = NeuralNetwork(784, 89, 10)
 # Load the last saved model if it exists
 if os.path.exists('model.pkl'):
@@ -33,7 +23,7 @@ if os.path.exists('model.pkl'):
 
 learning_rate = 0.5
 batch_size = 1024
-numberOfSteps = 1000
+numberOfSteps = 0
 
 # Create a figure for plotting
 fig = plt.figure()
@@ -52,25 +42,6 @@ print("Data loaded. Starting training...")
 start_time = time.time()
 
 for i in range(numberOfSteps):
-    step_start_time = time.time()
-
-    for j in range(len(batches)):
-        nn.learn(batches[j], learning_rate)
-
-    step_end_time = time.time()
-    step_duration = step_end_time - step_start_time
-
-    print(f"Step: {i}, Cost: {nn.total_cost(training_data)}, Time: {step_duration} seconds")
-
-end_time = time.time()
-total_duration = end_time - start_time
-print(f"Training complete. Total time: {total_duration} seconds")
-
-paint_root = tk.Tk()
-paint_app = ui.UserInterface(root=paint_root, training_data=training_data)
-paint_root.mainloop()
-
-for i in range(numberOfSteps):
     ax2.clear()
     for j in range(len(batches)):
         nn.learn(batches[j], learning_rate)
@@ -82,3 +53,11 @@ for i in range(numberOfSteps):
     learning_rate *= 0.95
     ax1.scatter(i, nn.total_cost(training_data), marker='x')
 plt.show()
+
+end_time = time.time()
+total_duration = end_time - start_time
+print(f"Training complete. Total time: {total_duration} seconds")
+
+paint_root = tk.Tk()
+paint_app = ui.UserInterface(root=paint_root, training_data=training_data)
+paint_root.mainloop()
