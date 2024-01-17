658e1778659f14b34e304507a4a40a7218c08f3e
Paul Frank <pawelspam42@gmail.com>
2024-01-17 15:37:02 +0100

    run python code on GPU

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,16 +1,20 @@
 import numpy as np
 import activation_functions as af
 import pickle
+from numba import cuda, jit
+
 
 class NeuralNetwork:
     def __init__(self, *nOfNodesInLayers):
         self.layers = [Layer(nOfNodesInLayers[i], nOfNodesInLayers[i + 1]) for i in range(len(nOfNodesInLayers) - 1)]
 
+    @jit
     def calculate_outputs(self, inputs):
         for layer in self.layers:
             inputs = layer.calculate_outputs(inputs)
         return inputs
-
+    
+    @jit
     def cost(self, data_point):
         outputs = self.calculate_outputs(data_point.inputs)
         output_layer = self.layers[-1]
@@ -20,15 +24,18 @@ class NeuralNetwork:
             cost += output_layer.node_cost(outputs[i], data_point.expected_outputs[i])
         return cost
 
+    @jit
     def total_cost(self, data):
         total_cost = sum(self.cost(data_point) for data_point in data)
         return total_cost / len(data)
 
+    @jit
     def learn(self, training_data, learning_rate):
         for data_point in training_data:
             self.backpropagate(data_point)
         self.apply_gradients(learning_rate)
 
+    @jit
     def backpropagate(self, data_point):
         outputs = self.calculate_outputs(data_point.inputs)
         deltas = [output - expected_output for output, expected_output in zip(outputs, data_point.expected_outputs)]
@@ -40,6 +47,7 @@ class NeuralNetwork:
             if i > 0 and i < len(self.layers) - 1:
                 deltas = np.dot(layer.weights, deltas) * af.sigmoid_derivative(layer.calculate_outputs(data_point.inputs))
 
+    @jit
     def apply_gradients(self, learning_rate):
         for layer in self.layers:
             if self.layers.index(layer) == 0:
@@ -68,19 +76,23 @@ class Layer:
         self.cost_gradient_b = np.zeros(self.num_nodes_out)
         self.weight_constant = 0.9
 
+    @jit
     def node_cost(self, output_activation, expected_output):
         error = expected_output - output_activation
         return error * error
 
+    @jit
     def calculate_outputs(self, inputs):
         weighted_inputs = np.dot(inputs, self.weights) + self.biases
         outputs = af.sigmoid(weighted_inputs)
         return outputs
 
+    @jit
     def calculate_gradients(self, inputs, deltas):
         self.cost_gradient_w = np.outer(inputs, deltas)
         self.cost_gradient_b = np.array(deltas)
 
+    @jit
     def apply_gradients(self, learning_rate):
         self.weights -= learning_rate * self.cost_gradient_w
         self.biases -= learning_rate * self.cost_gradient_b
